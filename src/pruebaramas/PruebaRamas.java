1fb328d7c777eca23df2a1379914475422bb5994
JMozar <jhon.moore@unmsm.edu.pe>
2022-11-10 15:14:10 -0500

    "commit en rama jhin"

--- src/pruebaramas/PruebaRamas.java
+++ src/pruebaramas/PruebaRamas.java
@@ -8,6 +8,7 @@ public class PruebaRamas {
     public static void main(String[] args) {
         System.out.println("Esto fue en main");
         System.out.println("Segundo commit en main");
+        System.out.println("Esto es en la rama local");
     }
     
 }
