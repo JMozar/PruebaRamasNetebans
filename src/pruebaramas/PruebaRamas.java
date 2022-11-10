fc13d3735e0c2afefe3479edeeba68e87f5ee71d
JMozar <jhon.moore@unmsm.edu.pe>
2022-11-10 15:06:34 -0500

    "2 commit en main"

--- src/pruebaramas/PruebaRamas.java
+++ src/pruebaramas/PruebaRamas.java
@@ -7,6 +7,7 @@ public class PruebaRamas {
     
     public static void main(String[] args) {
         System.out.println("Esto fue en main");
+        System.out.println("Segundo commit en main");
     }
     
 }
