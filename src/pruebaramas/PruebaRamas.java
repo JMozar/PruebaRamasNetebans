d3ca0a6ff298ef79e05e57a7769656bfbd3010c1
JMozar <jhon.moore@unmsm.edu.pe>
2022-11-10 15:43:39 -0500

    cambio luego del merge(solo en rama)

--- src/pruebaramas/PruebaRamas.java
+++ src/pruebaramas/PruebaRamas.java
@@ -14,6 +14,7 @@ public class PruebaRamas {
         System.out.println("Esto es en la rama local");
         System.out.println("Otro commit en rama");
         System.out.println("Otro cambio a la rama(luego del merge)");
+        System.out.println("Buenas tardes");
 
     }
     
